7652c6f3b040390a1cae56d55c01c0bb0d8d543a
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2025-02-07 22:20:48 +0100

    triangles

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -10,48 +10,78 @@ const HeroSection = () => {
     const { language } = useContext(LanguageContext);
 
     return (
-        <section
-            className={`hero-section overflow-x-hidden w-full flex justify-center items-start z-0 font-english  ${language === "ar" && "font-arabic"}   text-white text-center p-6 py-12 relative h-[90vh] `}
-            style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'right' }}
-        >
-            <div className="sm:w-1/4  md:w-1/2 ">
+        <>
+            <section
+                className={`hero-section overflow-hidden w-full flex justify-center items-start z-0 font-english ${language === "ar" && "font-arabic"} text-white text-center p-6 py-12 relative h-[90vh]`}
+                style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'right' }}
+            >
+                <div className="sm:w-1/4  md:w-1/2 ">
 
-            </div>
-            <div className=" w-full sm:w-3/4 md:2/3 lg:w-1/2  flex flex-col gap-6 justify-start  items-center">
-                <motion.img
-                    animate={{ x: [-50, 60, -50], opacity: [1, 0.7, 1] }}
-                    transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
-                    src={Logo}
-                    alt="Logo"
-                    className="h-28 w-40 mr-8 "
-                />
-
-                <div className='flex flex-col gap-4 mb-8'>
-                    <motion.h1
-                        whileInView={{ x: 0, opacity: 1 }}
-                        initial={{ x: 150, opacity: 0 }}
-                        transition={{ duration: 0.9 }}
-                        className='text-7xl font-bold'>{heroContent[language].title}</motion.h1>
-                    <motion.h2
-                        whileInView={{ x: 0, opacity: 1 }}
-                        initial={{ x: -150, opacity: 0 }}
-                        transition={{ duration: 0.9 }}
-                        className='text-5xl font-semibold text-secondary'>{heroContent[language].subtitle}</motion.h2>
                 </div>
+                <div className=" w-full sm:w-3/4 md:2/3 lg:w-1/2 overflow-hidden  flex flex-col gap-6 justify-start  items-center">
+                    <motion.img
+                        animate={{ x: [-50, 60, -50], opacity: [1, 0.7, 1] }}
+                        transition={{ duration: 2, repeat: Infinity, ease: "easeInOut" }}
+                        src={Logo}
+                        alt="Logo"
+                        className="h-28 w-40 mr-8 "
+                    />
+
+                    <div className='flex flex-col gap-4  mb-8'>
+                        <motion.h1
+                            whileInView={{ x: 0, opacity: 1 }}
+                            initial={{ x: 150, opacity: 0 }}
+                            transition={{ duration: 0.9 }}
+                            className='text-7xl font-bold'>{heroContent[language].title}</motion.h1>
+                        <motion.h2
+                            whileInView={{ x: 0, opacity: 1 }}
+                            initial={{ x: -150, opacity: 0 }}
+                            transition={{ duration: 0.9 }}
+                            className='text-5xl font-semibold text-secondary'>{heroContent[language].subtitle}</motion.h2>
+                    </div>
+                    <motion.div
+                        whileInView={{ y: 0, opacity: 1 }}
+                        initial={{ y: 60, opacity: 0 }}
+                        transition={{ duration: 0.9 }}
+                    >
+
+
+                        <Button
+
+                            variant="secondary" className='text-base font-semibold cursor-pointer  '>{heroContent[language].buttonText}</Button>
+
+                    </motion.div>
+                </div>
+            </section>
+
+            <div className="flex">
+                {/* Left Triangle */}
                 <motion.div
-                    whileInView={{ y: 0, opacity: 1 }}
-                    initial={{ y: 60, opacity: 0 }}
-                    transition={{ duration: 0.9 }}
-                >
+                    whileInView={{ x: 0, opacity: 1 }}
+                    initial={{ x: -60, opacity: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className="w-full h-[70px] bg-primary border-none outline-none"
+                    style={{
+                        clipPath: "polygon(0% 0%, 100% 0%, 0% 100%)",
+                        clipRule: "evenodd"
+                    }}
+                ></motion.div>
 
-
-                    <Button
-
-                        variant="secondary" className='text-base font-semibold cursor-pointer  '>{heroContent[language].buttonText}</Button>
-
-                </motion.div>
+                {/* Right Triangle */}
+                <motion.div
+                    whileInView={{ x: 0, opacity: 1 }}
+                    initial={{ x: 60, opacity: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className="w-1/2 h-[70px] bg-secondary border-none"
+                    style={{
+                        clipPath: "polygon(0% 0%, 100% 100%, 100% 0%)",
+                        clipRule: "evenodd"
+                    }}
+                ></motion.div>
             </div>
-        </section>
+
+
+        </>
     );
 };
 
