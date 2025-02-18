e1686bfa29aa8ce61e42e6ab9b26439fb1409735
barq-alsahab <barq.alsahab1@gmail.com>
2025-02-18 23:24:25 +0100

    secrit api key

--- src/components/sections/dashboard/CompaniesArchiveComp.jsx
+++ src/components/sections/dashboard/CompaniesArchiveComp.jsx
@@ -93,7 +93,7 @@ const CompaniesArchiveComp = () => {
   }, []);
 
   return (
-    <div className='flex flex-col items-center justify-center font-english px-2 py-3 md:p-6'>
+    <div className='flex flex-col items-center justify-center  px-2 py-3 md:p-6'>
       <h1 className='text-4xl font-bold text-center mb-4 text-primary'>
         Welcome to <span className='text-secondary'>Barq Al Sahab</span>
       </h1>
@@ -117,75 +117,75 @@ const CompaniesArchiveComp = () => {
         <p>Loading...</p>
       ) : filteredData.length === 0 ? (
         <div className='text-lg font-semibold text-gray-600 text-center'>
-            {filter === "All" && <p> There is no archived Company Submissions for now.</p>}
-            {filter === "Approved" && <p> There are no approved Company Submissions.</p>}
-            {filter === "Rejected" && <p> There are no rejected Company Submissions.</p>}
+          {filter === "All" && <p> There is no archived Company Submissions for now.</p>}
+          {filter === "Approved" && <p> There are no approved Company Submissions.</p>}
+          {filter === "Rejected" && <p> There are no rejected Company Submissions.</p>}
         </div>
       ) : (
         <>
           <div className='flex mx-auto w-full items-start justify-start max-h-96  text-[15px] overflow-auto'>
-                <table className='w-full overflow-auto bg-white'>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Company Name</TableHead>
-                <TableHead>Entity Type</TableHead>
-                <TableHead>Sector Type</TableHead>
-                <TableHead>Address</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Details</TableHead>
-                <TableHead>Submission Time</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {filteredData.map((company) => (
-                <TableRow key={company.id}>
-                  <TableCell>
-                    {company.companyName.length > 20 ? company.companyName.slice(0, 20) + "..." : company.companyName}
-                  </TableCell>
-                  <TableCell>
-                    {company.entityType.length > 15 ? company.entityType.slice(0, 15) + "..." : company.entityType}
-                  </TableCell>
-                  <TableCell>
-                    {company.sectorType.length > 15 ? company.sectorType.slice(0, 15) + "..." : company.sectorType}
-                  </TableCell>
-                  <TableCell>
-                    {company.address.length > 20 ? company.address.slice(0, 20) + "..." : company.address}
-                  </TableCell>
-                  <TableCell>
-                    <span className={`px-3 text-center py-1 text-[13px] font-semibold rounded-3xl text-white ${company.status === "Approved" ? "bg-green-500" : "bg-red-500"}`}>
-                      {company.status}
-                    </span>
-                  </TableCell>
-                  <TableCell>{company.submissionTime}</TableCell>
-                  <TableCell>
-                    <Button className='text-white font-semibold text-xs w-28 h-18'>
-                      <Link to={`/companies-archive/${company.id}`}>View Details</Link>
-                    </Button>
-                  </TableCell>
+            <table className='w-full overflow-auto bg-white'>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Company Name</TableHead>
+                  <TableHead>Entity Type</TableHead>
+                  <TableHead>Sector Type</TableHead>
+                  <TableHead>Address</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Details</TableHead>
+                  <TableHead>Submission Time</TableHead>
                 </TableRow>
+              </TableHeader>
+              <TableBody>
+                {filteredData.map((company) => (
+                  <TableRow key={company.id}>
+                    <TableCell>
+                      {company.companyName.length > 20 ? company.companyName.slice(0, 20) + "..." : company.companyName}
+                    </TableCell>
+                    <TableCell>
+                      {company.entityType.length > 15 ? company.entityType.slice(0, 15) + "..." : company.entityType}
+                    </TableCell>
+                    <TableCell>
+                      {company.sectorType.length > 15 ? company.sectorType.slice(0, 15) + "..." : company.sectorType}
+                    </TableCell>
+                    <TableCell>
+                      {company.address.length > 20 ? company.address.slice(0, 20) + "..." : company.address}
+                    </TableCell>
+                    <TableCell>
+                      <span className={`px-3 text-center py-1 text-[13px] font-semibold rounded-3xl text-white ${company.status === "Approved" ? "bg-green-500" : "bg-red-500"}`}>
+                        {company.status}
+                      </span>
+                    </TableCell>
+                    <TableCell>{company.submissionTime}</TableCell>
+                    <TableCell>
+                      <Button className='text-white font-semibold text-xs w-28 h-18'>
+                        <Link to={`/companies-archive/${company.id}`}>View Details</Link>
+                      </Button>
+                    </TableCell>
+                  </TableRow>
 
-              ))}
-            </TableBody>
-          </table>
-        </div>
+                ))}
+              </TableBody>
+            </table>
+          </div>
 
-      {/* Pagination Buttons */}
-      <div className="mt-4 flex justify-between w-full max-w-lg">
-        <Button
-          className="text-white font-semibold bg-primary text-xs w-28 disabled:opacity-50"
-          onClick={handleGetPreviousPage}
-          disabled={!firstDoc}
-        >
-          Previous Page
-        </Button>
-        <Button
-          className="text-white font-semibold text-xs w-28 disabled:opacity-50"
-          onClick={handleGetNextPage}
-          disabled={!lastDoc}
-        >
-          Next Page
-        </Button>
-      </div>
+          {/* Pagination Buttons */}
+          <div className="mt-4 flex justify-between w-full max-w-lg">
+            <Button
+              className="text-white font-semibold bg-primary text-xs w-28 disabled:opacity-50"
+              onClick={handleGetPreviousPage}
+              disabled={!firstDoc}
+            >
+              Previous Page
+            </Button>
+            <Button
+              className="text-white font-semibold text-xs w-28 disabled:opacity-50"
+              onClick={handleGetNextPage}
+              disabled={!lastDoc}
+            >
+              Next Page
+            </Button>
+          </div>
         </>
       )}
     </div>
